5d1b5b15c6866b1d440d806beb6e95a21911e506
Anna Xu <annaxu@Annas-MacBook-Pro-5.local>
2019-03-05 15:52:47 -0500

    heyyy

--- src/a5/PlateImpl.java
+++ src/a5/PlateImpl.java
@@ -105,7 +105,7 @@ public class PlateImpl implements Plate {
 		if (contents == null) {
 		return 0;
 		} else {
-		return Math.round(price - contents.getCost() * 100.0) / 100.0;
+		return Math.round((price - contents.getCost()) * 100.0) / 100.0;
 		}
 	}		
 
